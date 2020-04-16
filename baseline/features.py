c5708ef7cf128f9ce7421bb94ff1098bc0c6fcfc
sb505 <sb505@duke.edu>
2020-04-16 01:24:56 -0400

    For Basic version  feature extraction (User train_A)|non_emoji task A -OK | non_emoji task B test data needed in correct format| need to read gold labels

--- baseline/features.py
+++ baseline/features.py
@@ -1,4 +1,10 @@
-from .utils import read_tweets
+import pickle
+
+from ..utils import read_non_emoji_train_tweets
+from ..utils import read_non_emoji_test_tweets
+
+
+
 
 def extract_ngrams():
     pass
@@ -10,8 +16,18 @@ def dependency():
     pass
 
 if __name__ == '__main__':
-    fn = 'train/SemEval2018-T3-train-taskA_emoji.txt'
-    read_tweets()
+    fp_train_A = 'tweet_irony_detection/train/SemEval2018-T3-train-taskA.txt'
+    fp_test_A='tweet_irony_detection/test_TaskA/SemEval2018-T3_input_test_taskA.txt'
+    fp_train_B = 'tweet_irony_detection/train/SemEval2018-T3-train-taskB.txt'
+    # fp_test_B = 'tweet_irony_detection/test_TaskB/SemEval2018-T3_input_test_taskB.txt'
+
+    train_A=read_non_emoji_train_tweets(fp_train_A)
+    test_A=read_non_emoji_test_tweets(fp_test_A)
+    train_B = read_non_emoji_train_tweets(fp_train_B)
+    # test_B = read_non_emoji_test_tweets(fp_test_B)
+
+    print(len(train_A),len(train_B),len(test_A),len(test_A))
+
 
     # unit test for features
-    extract_ngrams()
+    # extract_ngrams()
--- utils/__init__.py
+++ utils/__init__.py
@@ -1,4 +1,45 @@
+from typing import Dict, List, NamedTuple
+from nltk.tokenize import word_tokenize
 
-# TODO: better define later
-def read_tweets():
-    pass
+class Tweet(NamedTuple):
+    tweet_id: int
+    tweet_label:int
+    tweet_text:str
+    # emojis:List[str]
+
+    def tweet_words(self):
+        return word_tokenize(self.tweet_text)
+
+    def __repr__(self):
+        return (f"tweet_id: {self.tweet_id}\n" +
+                f"tweet_label: {self.tweet_label}\n" +
+                f"tweet_text: {self.tweet_text}")
+
+
+def read_non_emoji_train_tweets(fp):
+    tweets=[]
+    with open(fp,encoding='utf8') as f:
+        for line in f:
+            line=line.strip()
+            if not line.lower().startswith("tweet"):
+                split=line.split("\t")
+                id=int(split[0])
+                label=int(split[1])
+                text=split[2]
+
+
+                tweets.append(Tweet(id,label,text))
+    return tweets
+
+def read_non_emoji_test_tweets(fp):
+    tweets=[]
+    with open(fp,encoding='utf8') as f:
+        for line in f:
+            line=line.strip()
+            if not line.lower().startswith("tweet"):
+                split=line.split("\t")
+                id=int(split[0])
+                label=None
+                text=split[1]
+                tweets.append(Tweet(id,label,text))
+    return tweets
