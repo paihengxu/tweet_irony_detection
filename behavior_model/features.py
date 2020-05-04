5513da4779bc963c6211d67a16c4ff6121ae3a35
Paiheng-Xu <38190298+Paiheng-Xu@users.noreply.github.com>
2020-05-03 20:37:46 -0400

    update senti_vocab and twitter_scraper

--- behavior_model/features.py
+++ behavior_model/features.py
@@ -6,16 +6,19 @@ from utils import read_non_emoji_tweets
 from utils import get_label
 from utils import print_class_stats
 from utils import read_vocabulary_with_occurrence, write_tokens_to_txt
+from utils import write_dict_to_json, read_dict_from_json
+
 
 ### DEFINE CONST HERE
 
 def twitter_sentiment_score(corpus, n):
     """
     input: whole dataset, n indicating n-gram
-    output: two dictionaries, key: tweet_id, value: 1-dimensional binary numpy array
+    output: two dictionaries, key: tweet_id, value: 1-dimensional numpy array
     Done: Out of Vocabulary (OOV) words
     """
-    if not os.path.exists('behavior_model/positive_{}_vocab.txt'.format(n)) or not os.path.exists('behavior_model/negative_{}_vocab.txt'.format(n)):
+    if not os.path.exists('behavior_model/positive_{}_vocab.txt'.format(n)) or not os.path.exists(
+        'behavior_model/negative_{}_vocab.txt'.format(n)):
         os.system('python -m behavior_model.sent_vocab')
 
     # key: word, value: index
@@ -29,10 +32,10 @@ def twitter_sentiment_score(corpus, n):
         _id = data.tweet_id
         for idx, ele in enumerate(lower_tokens):
             # check idx for n-grams
-            if idx >= len(lower_tokens) - (n-1):
+            if idx >= len(lower_tokens) - (n - 1):
                 continue
             # set default value as 0, bc (-0.1, 0.1) is filtered out
-            n_gram = [lower_tokens[w_idx] for w_idx in range(idx, idx+n)]
+            n_gram = [lower_tokens[w_idx] for w_idx in range(idx, idx + n)]
             assert len(n_gram) == n
             pos_score = pos_vocab.get(tuple(n_gram), 0)
             neg_score = neg_vocab.get(tuple(n_gram), 0)
@@ -40,7 +43,7 @@ def twitter_sentiment_score(corpus, n):
             if pos_score + neg_score == 0:
                 continue
 
-            senti_score = (pos_score - neg_score)/(pos_score + neg_score)
+            senti_score = (pos_score - neg_score) / (pos_score + neg_score)
 
             if -0.1 < senti_score < 0.1:
                 continue
@@ -49,19 +52,37 @@ def twitter_sentiment_score(corpus, n):
 
     return senti_features
 
+
 if __name__ == '__main__':
-    ### read in corpus
     fp_train_A = 'train/SemEval2018-T3-train-taskA.txt'
+    fp_train_B = 'train/SemEval2018-T3-train-taskB.txt'
+    fp_test_A = 'test_TaskA/SemEval2018-T3_input_test_taskA.txt'
+    fp_test_B = 'test_TaskB/SemEval2018-T3_input_test_taskB.txt'
+    fp_labels_A = 'goldtest_TaskA/SemEval2018-T3_gold_test_taskA_emoji.txt'
+    fp_labels_B = 'goldtest_TaskB/SemEval2018-T3_gold_test_taskB_emoji.txt'
+
+    ### read in corpus
     pre_process_url = True  # Set to remove URLs
     pre_process_usr = True
     train_A = read_non_emoji_tweets(fp_train_A, "train", pre_process_url, pre_process_usr)
+    train_B = read_non_emoji_tweets(fp_train_B, "train", pre_process_url, pre_process_usr)
+    test_A = read_non_emoji_tweets(fp_test_A, "test", pre_process_url, pre_process_usr)
+    test_B = read_non_emoji_tweets(fp_test_B, "test", pre_process_url, pre_process_usr)
+
+    name_2_dataset = {
+        'train_A': train_A,
+        'train_B': train_B,
+        'test_A': test_A,
+        'test_B': test_B
+    }
 
     ### test sentiment score
-    senti_score_bigram = twitter_sentiment_score(train_A, 2)
-    senti_score_trigram = twitter_sentiment_score(train_A, 3)
+    for name, dataset in name_2_dataset.items():
+        feature_2 = twitter_sentiment_score(dataset, 2)
+        feature_3 = twitter_sentiment_score(dataset, 3)
 
-    for i in range(1, 20):
-        assert i==train_A[i-1].tweet_id
-        print(senti_score_bigram[i])
-        print(senti_score_trigram[i])
-        print(train_A[i-1].tweet_text)
+        write_dict_to_json(feature_2, fn='{dataset}_{feature_name}.json.gz'.format(dataset=name,
+                                                                                   feature_name='senti_bigram'))
+
+        write_dict_to_json(feature_3, fn='{dataset}_{feature_name}.json.gz'.format(dataset=name,
+                                                                                   feature_name='senti_trigram'))
--- twitter_scrape/scraper.py
+++ twitter_scrape/scraper.py
@@ -1,34 +1,67 @@
 import tweepy
+from tweepy.streaming import StreamListener, Stream
+from http.client import IncompleteRead
+import traceback
 import codecs
 import pandas as pd
 import time
+import sys
 
-def scrape_hashtags(query, outfn, label):
-    #### input your credentials here
-    consumer_key = 'Q8NYmNp0IH5sw7GRkmOQ'
-    consumer_secret = 'mZoaRAXIA6k4aLbjRHAC9pw49hv33WlTdzQvkTWZkI'
-    access_token = '483153462-roeo2ntKhmNNOJt54KjYMzJgJmJJvgnW5Xe5cLmm'
-    access_token_secret = 'TrmnImRZEfQ1AprXyYghylg2kHLz4X3eh3H7nO40Y'
+#### input your credentials here
+consumer_key = 'Q8NYmNp0IH5sw7GRkmOQ'
+consumer_secret = 'mZoaRAXIA6k4aLbjRHAC9pw49hv33WlTdzQvkTWZkI'
+access_token = '483153462-roeo2ntKhmNNOJt54KjYMzJgJmJJvgnW5Xe5cLmm'
+access_token_secret = 'TrmnImRZEfQ1AprXyYghylg2kHLz4X3eh3H7nO40Y'
 
-    ### get auth
+
+def limit_handled(cursor):
+    while True:
+        try:
+            yield cursor.next()
+        except tweepy.RateLimitError:
+            print('sleeping')
+            time.sleep(15 * 60)
+
+
+def getTimeString():
+    t = time.localtime()
+    return '%d-%d-%d-%d-%d-%d' % (t.tm_year, t.tm_mon, t.tm_mday, t.tm_hour, t.tm_min, t.tm_sec)
+
+
+def get_api():
     auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
     auth.set_access_token(access_token, access_token_secret)
     api = tweepy.API(auth, wait_on_rate_limit=True)
+    return api
+
+
+def scrape_hashtags_cursor(query, outfn, label):
+    '''
+    cursor raise RuntimeError: generator raised StopIteration
+    '''
+
+    ### get auth
+    api = get_api()
 
     scraped_ids = set()
-    #Use csv Writer
+    # Use csv Writer
     with codecs.open(outfn, 'w', encoding='utf8') as outf:
         outf.write('{}\n'.format('\t'.join(['Tweet index', 'Label', 'Tweet text'])))
         idx = 0
-        p = 1
         while True:
             try:
+                # for tweet in limit_handled(tweepy.Cursor(api.search,
+                #                                          q=query,
+                #                                          tweet_mode="extended",
+                #                                          since='2017-11-25',
+                #                                          lang="en",
+                #                                          ).items()):
                 for tweet in tweepy.Cursor(api.search,
                                            q=query,
                                            tweet_mode="extended",
                                            since='2017-11-25',
-                                           count=400,
                                            lang="en",
+                                           count=400
                                            ).items():
                     tweet_id = tweet.id_str
                     if tweet_id in scraped_ids:
@@ -38,27 +71,81 @@ def scrape_hashtags(query, outfn, label):
                     text = tweet.full_text.replace('\n', ' ').replace('\r', ' ')
                     outf.write('{}\n'.format('\t'.join([str(idx), label, text])))
                     idx += 1
-                    if idx > 400000:
+                    if idx % 6000 == 0:
+                        print('get number of tweets:', len(scraped_ids))
+                        time.sleep(900)
+                    if idx > 50000:
                         break
-            except Exception as err:
+            except StopIteration as err:
                 print(err)
+                # time.sleep(1800)
                 break
-            print('get number of tweets:', len(scraped_ids))
-            p += 1
-            time.sleep(900)
-            # csvWriter.writerow([tweet.created_at, tweet.full_text.encode('utf-8')])
+
+
+class MyStreamListener(StreamListener):
+    def on_status(self, status):
+        print(status.created_at, status.text)
+
+    def on_error(self, status_code):
+        if status_code == 420:
+            return False
+
+
+class KeywordStreamer:
+    '''
+    Streams in tweets matching a set of terms/from a set of user streams.
+    '''
+
+    def __init__(self, kw_list):
+
+        self.kws = kw_list
+        self.api = get_api()
+        self.stream = Stream(auth=self.api.auth, listener=MyStreamListener())
+
+    def streamTweets(self):
+        goForIt = True
+        while goForIt:
+            try:
+                goForIt = False
+                self.stream.filter(track=self.kws)
+            except IncompleteRead as ex:
+                try:
+                    tb = traceback.format_exc()
+                except:
+                    tb = ''
+                print("%s: %s\n%s\n\n" % (getTimeString(),
+                                          str(sys.exc_info()) + "\nStill ticking!", tb))
+                time.sleep(15 * 60)
+                goForIt = True
+            except:
+                try:
+                    tb = traceback.format_exc()
+                except:
+                    tb = ''
+                print('%s: %s\n%s\n\n' % (getTimeString(),
+                                          str(sys.exc_info()), tb))
+                time.sleep(15 * 60)
+                goForIt = True
+
 
 if __name__ == '__main__':
     positive_hashtags = ['#positive', '#love', '#motivation', '#positivevibes', '#happy',
                          '#amazing', '#positivity', '#inspiration', '#happiness', '#success']
-    positive_query = "{} -filter:retweets".format(' OR '.join(positive_hashtags)) # :)
+    positive_query = "{} -filter:retweets".format(' OR '.join(positive_hashtags))  # :)
     print(positive_query)
     negative_hashtags = ['#sad', '#negativespace', '#negative', '#depressed', '#depression',
                          '#hate', '#frustrated', '#pain', '#struggle', '#failure']
     negative_query = "{} -filter:retweets".format(' OR '.join(negative_hashtags))  # :(
     print(negative_query)
+
+    ### use cursor
     # label 0 for positive, 1 for negative
-    scrape_hashtags(positive_query, 'twitter_scrape/positive_text.txt', label='0')
+    scrape_hashtags_cursor(positive_query, 'twitter_scrape/positive_text.txt', label='0')
     print('done positive')
-    scrape_hashtags(negative_query, 'twitter_scrape/negative_text.txt', label='1')
+    time.sleep(900)
+    scrape_hashtags_cursor(negative_query, 'twitter_scrape/negative_text.txt', label='1')
     print('done negative')
+
+    ### use stream
+    # streamer = KeywordStreamer(negative_hashtags)
+    # streamer.streamTweets()
