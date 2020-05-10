67b94043417bb9d4039efa41c2ead3205599ab91
jxing8 <48233609+jxing8@users.noreply.github.com>
2020-05-10 12:32:06 -0700

    Added skipgram and cbow

---
+++ word_representations/skipgram.py
@@ -0,0 +1,83 @@
+# importing all necessary modules 
+from nltk.tokenize import sent_tokenize, word_tokenize 
+import warnings 
+  
+warnings.filterwarnings(action = 'ignore')
+
+import numpy as np
+
+from utils import read_non_emoji_tweets
+from utils import write_dict_to_json
+  
+import gensim 
+from gensim.models import Word2Vec
+
+
+def create_corpus(data):
+    corpus = []
+    for tweet in data:
+        tokenized = tweet.tweet_words()
+        corpus.append(tokenized)
+    return corpus
+
+
+def skipgram_embedding(data):
+    '''
+    data : (Tweet : namedTuple) list
+
+    # Return
+    dict : 
+        tweet_id : int -> nparray : [300 x 1]
+    
+    '''
+    feature_dict = {}
+    try:
+        corpus = create_corpus(data)
+        
+        # Create Skip Gram model 
+        model = gensim.models.Word2Vec(corpus, min_count = 1, size = 100, window = 5, sg = 1)
+        
+        for tweet in data:
+
+            # tokenize and tag tweet
+            tokenized = tweet.tweet_words()
+            
+            embedding_min = model[tokenized].min(axis=0)
+            embedding_max = model[tokenized].max(axis=0)
+            embedding_mean = model[tokenized].mean(axis=0)
+            
+            feature_dict[tweet.tweet_id] = np.concatenate([embedding_min, embedding_max, embedding_mean])
+            
+        return feature_dict
+
+    except Exception as e:
+        print("In Skip Gram Exceptions")
+        print(str(e))
+
+
+if __name__ == '__main__':
+    
+    fp_train_A = 'train/SemEval2018-T3-train-taskA.txt'
+    fp_train_B = 'train/SemEval2018-T3-train-taskB.txt'
+    fp_test_A = 'test_TaskA/SemEval2018-T3_input_test_taskA.txt'
+    fp_test_B = 'test_TaskB/SemEval2018-T3_input_test_taskB.txt'
+    
+    pre_process_url = True  # Set to remove URLs
+    pre_process_usr = True
+    train_A = read_non_emoji_tweets(fp_train_A, "train", pre_process_url, pre_process_usr)
+    train_B = read_non_emoji_tweets(fp_train_B, "train", pre_process_url, pre_process_usr)
+    
+    test_A = read_non_emoji_tweets(fp_test_A, "test", pre_process_url, pre_process_usr)
+    test_B = read_non_emoji_tweets(fp_test_B, "test", pre_process_url, pre_process_usr)
+    
+    train_A_skipgram = skipgram_embedding(train_A)
+    write_dict_to_json(train_A_skipgram, 'train_A_skipgram.json.gz')
+    
+    train_B_skipgram = skipgram_embedding(train_B)
+    write_dict_to_json(train_B_skipgram, 'train_B_skipgram.json.gz')
+    
+    test_A_skipgram = skipgram_embedding(test_A)
+    write_dict_to_json(test_A_skipgram, 'test_A_skipgram.json.gz')
+    
+    test_B_skipgram = skipgram_embedding(test_B)
+    write_dict_to_json(test_B_skipgram, 'test_B_skipgram.json.gz')
